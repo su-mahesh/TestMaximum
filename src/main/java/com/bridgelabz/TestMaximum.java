68d98a1204cc8f1f2329e346b41b2e7e68d9d91d
su-mahesh <maheshkangude@gmail.com>
2021-01-24 15:11:16 +0530

    REFACTORED: all three methods to one generic

--- src/main/java/com/bridgelabz/TestMaximum.java
+++ src/main/java/com/bridgelabz/TestMaximum.java
@@ -2,30 +2,12 @@ package com.bridgelabz;
 
 public class TestMaximum {
 
-    public Integer testMaxInteger(Integer num1, Integer num2, Integer num3) {
+    public <E extends Comparable> E testMaximum(E input1, E input2, E input3) {
 
-        if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-            return num1;
-        if(num2.compareTo(num3) > 0)
-            return num2;
-        return num3;
-    }
-
-    public Float testMaxFloat(Float num1, Float num2, Float num3) {
-
-        if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-            return num1;
-        if(num2.compareTo(num3) > 0)
-            return num2;
-        return num3;
-    }
-
-    public String testMaxString(String string1, String string2, String string3) {
-
-        if(string1.compareTo(string2) > 0 && string1.compareTo(string3) > 0)
-            return string1;
-        if (string2.compareTo(string3) > 0)
-            return string2;
-        return string3;
+        if(!(input1.compareTo(input2) <= 0) && input1.compareTo(input3) > 0)
+            return input1;
+        if(input2.compareTo(input3) > 0)
+            return input2;
+        return input3;
     }
 }
--- src/test/java/com/bridgelabz/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/TestMaximumTest.java
@@ -9,58 +9,58 @@ public class TestMaximumTest {
 
     @Test
     public void givenMaxIntegerAt_FirstPosition_shouldReturnSameNumber(){
-        Integer result = testMaximum.testMaxInteger(34, 12, 23);
+        Integer result = testMaximum.testMaximum(34, 12, 23);
         Assert.assertEquals(34, result.intValue());
     }
 
     @Test
     public void givenMaxIntegerAt_SecondPosition_shouldReturnSameNumber(){
-        Integer result = testMaximum.testMaxInteger(34, 87, 23);
+        Integer result = testMaximum.testMaximum(34, 87, 23);
         Assert.assertEquals(87, result.intValue());
     }
 
     @Test
     public void givenMaxIntegerAt_ThirdPosition_shouldReturnSameNumber(){
-        Integer result = testMaximum.testMaxInteger(34, 67, 89);
+        Integer result = testMaximum.testMaximum(34, 67, 89);
         Assert.assertEquals(89, result.intValue());
     }
 
     @Test
     public void givenMaxFloatAt_FirstPosition_shouldReturnSameNumber(){
         Float num1 = 754.5f, num2 = 534.6f, num3 = 234.42f;
-        Float result = testMaximum.testMaxFloat(num1, num2, num3);
+        Float result = testMaximum.testMaximum(num1, num2, num3);
        Assert.assertEquals((Float)754.5f, result);
     }
 
     @Test
     public void givenMaxFloatAt_SecondPosition_shouldReturnSameNumber(){
         Float num1 = 754.5f, num2 = 5344.6f, num3 = 234.42f;
-        Float result = testMaximum.testMaxFloat(num1, num2, num3);
+        Float result = testMaximum.testMaximum(num1, num2, num3);
         Assert.assertEquals(num2, result);
     }
 
     @Test
     public void givenMaxFloatAt_ThirdPosition_shouldReturnSameNumber(){
         Float num1 = 754.5f, num2 = 344.6f, num3 = 3234.42f;
-        Float result = testMaximum.testMaxFloat(num1, num2, num3);
+        Float result = testMaximum.testMaximum(num1, num2, num3);
         Assert.assertEquals(num3, result);
     }
 
     @Test
     public void givenMaxStringAt_FirstPosition_shouldReturnSameString(){
-        String result = testMaximum.testMaxString("zebra", "Lion", "Tiger");
+        String result = testMaximum.testMaximum("zebra", "Lion", "Tiger");
         Assert.assertEquals("zebra", result);
     }
 
     @Test
     public void givenMaxStringAt_SecondPosition_shouldReturnSameString(){
-        String result = testMaximum.testMaxString("Zebra", "zebra", "Tiger");
+        String result = testMaximum.testMaximum("Zebra", "zebra", "Tiger");
         Assert.assertEquals("zebra", result);
     }
 
     @Test
     public void givenMaxStringAt_ThirdPosition_shouldReturnSameString(){
-        String result = testMaximum.testMaxString("lion", "tiger", "zebra");
+        String result = testMaximum.testMaximum("lion", "tiger", "zebra");
         Assert.assertEquals("zebra", result);
     }
 }
