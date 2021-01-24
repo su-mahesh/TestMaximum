88220b76c76c34703f2b766c6998a27691014119
su-mahesh <maheshkangude@gmail.com>
2021-01-24 13:46:47 +0530

    ADDED: given max at first position should return max float

--- src/main/java/com/bridgelabz/TestMaximum.java
+++ src/main/java/com/bridgelabz/TestMaximum.java
@@ -2,7 +2,16 @@ package com.bridgelabz;
 
 public class TestMaximum {
 
-    public Integer testMaximum(Integer num1, Integer num2, Integer num3) {
+    public Integer testMaxInteger(Integer num1, Integer num2, Integer num3) {
+
+        if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+            return num1;
+        if(num2.compareTo(num3) > 0)
+            return num2;
+        return num3;
+    }
+
+    public Float testMaxFloat(Float num1, Float num2, Float num3) {
 
         if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
             return num1;
--- src/test/java/com/bridgelabz/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/TestMaximumTest.java
@@ -8,21 +8,27 @@ public class TestMaximumTest {
     TestMaximum testMaximum = new TestMaximum();
 
     @Test
-    public void givenMaxNumberAt_FirstPosition_shouldReturnSameNumber(){
-        Integer result = testMaximum.testMaximum(34, 12, 23);
+    public void givenMaxIntegerAt_FirstPosition_shouldReturnSameNumber(){
+        Integer result = testMaximum.testMaxInteger(34, 12, 23);
         Assert.assertEquals(34, result.intValue());
     }
 
     @Test
-    public void givenMaxNumberAt_SecondPosition_shouldReturnSameNumber(){
-        Integer result = testMaximum.testMaximum(34, 87, 23);
+    public void givenMaxIntegerAt_SecondPosition_shouldReturnSameNumber(){
+        Integer result = testMaximum.testMaxInteger(34, 87, 23);
         Assert.assertEquals(87, result.intValue());
     }
 
     @Test
-    public void givenMaxNumberAt_ThirdPosition_shouldReturnSameNumber(){
-        Integer result = testMaximum.testMaximum(34, 67, 89);
+    public void givenMaxIntegerAt_ThirdPosition_shouldReturnSameNumber(){
+        Integer result = testMaximum.testMaxInteger(34, 67, 89);
         Assert.assertEquals(89, result.intValue());
+    }
+
+    @Test
+    public void givenMaxFloatAt_FirstPosition_shouldReturnSameNumber(){
+        Float result = testMaximum.testMaxFloat(754.5f, 534.6f, 234.42f);
+        Assert.assertEquals((Float)754.5f, result);
     }
 }
 
