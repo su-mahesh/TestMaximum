982d989900b38cd0eadf3dcbf645a71e0df030ad
su-mahesh <maheshkangude@gmail.com>
2021-01-25 14:25:10 +0530

    REFACTORED: test methods names improvised

--- src/test/java/com/bridgelabz/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/TestMaximumTest.java
@@ -6,28 +6,28 @@ import org.junit.Test;
 public class TestMaximumTest {
 
     @Test
-    public void givenMaxIntegerAt_FirstPosition_shouldReturnSameNumber(){
+    public void givenIntegerParameters_WhenMaxValueAtFirstPosition_shouldReturnMaxInteger(){
         TestMaximum<Integer> testMaximum = new TestMaximum<>(34, 12, 23);
         Integer result = testMaximum.getMaximum();
         Assert.assertEquals(34, result.intValue());
     }
 
     @Test
-    public void givenMaxIntegerAt_SecondPosition_shouldReturnSameNumber(){
+    public void givenIntegerParameters_WhenMaxValueAtSecondPosition_shouldReturnMaxInteger(){
         TestMaximum<Integer> testMaximum = new TestMaximum<>(34, 87, 23);
         Integer result = testMaximum.getMaximum();
         Assert.assertEquals(87, result.intValue());
     }
 
     @Test
-    public void givenMaxIntegerAt_ThirdPosition_shouldReturnSameNumber(){
+    public void givenIntegerParameters_WhenMaxValueAtThirdPosition_shouldReturnMaxInteger(){
         TestMaximum<Integer> testMaximum = new TestMaximum<>(34, 67, 89);
         Integer result = testMaximum.getMaximum();
         Assert.assertEquals(89, result.intValue());
     }
 
     @Test
-    public void givenMaxFloatAt_FirstPosition_shouldReturnSameNumber(){
+    public void givenFloatParameters_WhenMaxValueAtFirstPosition_shouldReturnMaxFloat(){
         Float num1 = 754.5f, num2 = 534.6f, num3 = 234.42f;
         TestMaximum<Float> testMaximum = new TestMaximum<>(num1, num2, num3);
 
@@ -36,7 +36,7 @@ public class TestMaximumTest {
     }
 
     @Test
-    public void givenMaxFloatAt_SecondPosition_shouldReturnSameNumber(){
+    public void givenFloatParameters_WhenMaxValueAtSecondPosition_shouldReturnMaxFloat(){
         Float num1 = 754.5f, num2 = 5344.6f, num3 = 234.42f;
         TestMaximum<Float> testMaximum = new TestMaximum<>(num1, num2, num3);
         Float result = testMaximum.getMaximum();
@@ -44,7 +44,7 @@ public class TestMaximumTest {
     }
 
     @Test
-    public void givenMaxFloatAt_ThirdPosition_shouldReturnSameNumber(){
+    public void givenFloatParameters_WhenMaxValueAtThirdPosition_shouldReturnMaxFloat(){
         Float num1 = 754.5f, num2 = 344.6f, num3 = 3234.42f;
         TestMaximum<Float> testMaximum = new TestMaximum<>(num1, num2, num3);
         Float result = testMaximum.getMaximum();
@@ -52,21 +52,21 @@ public class TestMaximumTest {
     }
 
     @Test
-    public void givenMaxStringAt_FirstPosition_shouldReturnSameString(){
+    public void givenStringParameters_WhenMaxValueAtFirstPosition_shouldReturnMaxString(){
         TestMaximum<String> testMaximum = new TestMaximum<>("zebra", "Lion", "Tiger");
         String result = testMaximum.getMaximum();
         Assert.assertEquals("zebra", result);
     }
 
     @Test
-    public void givenMaxStringAt_SecondPosition_shouldReturnSameString(){
+    public void givenStringParameters_WhenMaxValueAtSecondPosition_shouldReturnMaxString(){
         TestMaximum<String> testMaximum = new TestMaximum<>("Zebra", "zebra", "Tiger");
         String result = testMaximum.getMaximum();
         Assert.assertEquals("zebra", result);
     }
 
     @Test
-    public void givenMaxStringAt_ThirdPosition_shouldReturnSameString(){
+    public void givenStringParameters_WhenMaxValueAtThirdPosition_shouldReturnMaxString(){
         TestMaximum<String> testMaximum = new TestMaximum<>("lion", "tiger", "zebra");
         String result = testMaximum.getMaximum();
         Assert.assertEquals("zebra", result);
